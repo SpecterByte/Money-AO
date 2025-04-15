7b468b13c7985121505506960d8d1219561d71fb
SpecterByte <specterbyte.goat@gmail.com>
2025-04-15 01:35:43 +0100

    O melhor site de todos

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+var nome = prompt('Nome')
+var idade = prompt('idade')
+  document.getElementById("meuBotao").onclick = function() {
+    alert("Você clicou no botão!");
+    var audio = new Audio('/ahm.wav');
+audio.play();
+  }
+  
